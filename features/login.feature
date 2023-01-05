3b16e0badd5439541461545d3c550b79684539a2
jdanielue <2360@holbertonschool.com>
2023-01-05 17:03:31 -0500

    adding new scenario: login form

--- features/login.feature
+++ features/login.feature
@@ -2,6 +2,14 @@ Feature: Swag Labs -login
 
 Scenario: the usernames and passwords are visible
 
-  Given I join the swag labs page home page
-  Then the user is "standard_user"
+  Given the user joins the Swag labs page home page
+  Then the user is "Accepted usernames are:"
   And the password label for all users exists
+
+Scenario: the login form is visible
+
+  Given the user is on the homepage "https://www.saucedemo.com"
+  When the user types the username "standard_user"
+  When the user types the password "secret_sauce"
+  When the user clicks on submit
+  Then the user joins the inventory page "https://www.saucedemo.com/inventory.html"
--- step_definitions/login.js
+++ step_definitions/login.js
@@ -1,14 +1,38 @@
 const { client } = require('nightwatch-api');
-const {Given, Then} = require('cucumber');
+const {Given, When, Then} = require('cucumber');
 
-Given(/^I join the swag labs page home page$/, () => {
-  return client.url('https://www.saucedemo.com/').waitForElementVisible('body', 1000);
+Given(/^the user joins the Swag labs page home page$/, () => {
+  return client.init().waitForElementVisible('body', 1000);
 });
 
 Then(/^the user is "([^"]*)"$/, userName => {
-  return client.assert.textEquals('//*[@id="login_credentials"]/text()[1]', userName);
+  return client.expect.element('#login_credentials > h4').text.to.equals(userName);
 });
 
 Then(/^the password label for all users exists$/, () => {
-  return client.assert.visible('.login_password');
+  return client.expect.element('.login_password').to.be.present;
+});
+
+Given(/the user is on the homepage "([^"]*)"$/, (homepageURL) => {
+  return client.expect.url().to.contain(homepageURL);
+});
+
+When(/^the user types the username "([^"]*)"$/, userName => {
+  return client
+  .waitForElementVisible('#user-name')
+  .setValue('#user-name', userName);
+});
+
+When(/^the user types the password "([^"]*)"$/, password => {
+  return client
+  .waitForElementVisible('#password')
+  .setValue('#password', password);
+});
+
+When(/^the user clicks on submit$/, () => {
+  return client.click('#login-button');
+});
+
+Then(/^the user joins the inventory page "([^"]*)"$/, inventoryURL => {
+  return client.expect.url().to.contain(inventoryURL);
 });
