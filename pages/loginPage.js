9d44e0a4d909e7749d1ca7a1603c01769c94d258
jdanielue <2360@holbertonschool.com>
2023-01-06 18:45:07 -0500

    ading login function

--- pages/loginPage.js
+++ pages/loginPage.js
@@ -1,10 +1,27 @@
+const elements =  {
+  availableUsersNames: '#login_credentials',
+  availablePasswords: '.login_password',
+  userNameInput: '#user-name',
+  passwordInput: '#password',
+  submitButton: '#login-button',
+  errorText: 'h3',
+}
+
+const loginUserFunc = function loginUser(client, userName, password) {
+
+  return client
+  .init().waitForElementVisible('body', 1000)
+  .waitForElementVisible(elements.userNameInput)
+  .clearValue(elements.userNameInput)
+  .setValue(elements.userNameInput, userName)
+  .waitForElementVisible(elements.passwordInput)
+  .clearValue(elements.passwordInput)
+  .setValue(elements.passwordInput, password)
+  .click(elements.submitButton);
+
+}
+
 module.exports = {
-  elements: {
-    availableUsersNames: '#login_credentials',
-    availablePasswords: '.login_password',
-    userNameInput: '#user-name',
-    passwordInput: '#password',
-    submitButton: '#login-button',
-    errorText: 'h3',
-  }
+  loginUserFunc,
+  elements
 };
