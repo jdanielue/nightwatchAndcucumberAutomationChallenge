e50052ced5f782f316ffd4f0e2e7d89c1032525b
jdanielue <2360@holbertonschool.com>
2023-01-09 15:28:52 -0500

    adding POM and step definitions for complete page

---
+++ pages/completePage.js
@@ -0,0 +1,7 @@
+const elements = {
+  gratitudeMessage : 'h2.complete-header',
+}
+
+module.exports = {
+  elements
+};
---
+++ step_definitions/complete.js
@@ -0,0 +1,11 @@
+const { client } = require('nightwatch-api');
+const {Given, When, Then} = require('cucumber');
+const completePage = require('../pages/completePage.js');
+
+When(/^the user is on the complete page "([^"]*)"$/, (completePageURL) => {
+  return client.expect.url().to.contain(completePageURL);
+});
+
+Then(/^the user is able to see the gratitude message "([^"]*)"$/, (thankyouLabel) => {
+  return client.expect.element(completePage.elements.gratitudeMessage).to.have.text.that.equals(thankyouLabel);
+});
